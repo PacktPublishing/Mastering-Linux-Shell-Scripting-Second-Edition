e23193afe45724a282c7fb60c800ef575b0ba296
Eric roth <solaimanshah059@gmail.com>
2019-12-22 08:47:10 -0500

    improved

--- Chapter01/Variable scope script1.sh
+++ Chapter01/Variable scope script1.sh
@@ -4,5 +4,5 @@ name="Mokhtar"
 export name # The variable will be accessible to other processes
 ./06-script2.sh 
 
-# second part, we updated name variable in script2.h
+# second part, we updated name variable in script2.sh, let's what we see
 echo $name # ouput: Mokhtar
