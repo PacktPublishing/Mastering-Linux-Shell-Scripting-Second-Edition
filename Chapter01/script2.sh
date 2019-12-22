0de701828b3c2c51c971d4bf40c248f0e3b40571
Eric Roth <28618316+lifeeric@users.noreply.github.com>
2019-12-22 08:33:39 -0500

    Add in order to grasp it easily
    I was confused because of the copy part, so now I make it a bit possible to grasp easily!
    
    Please, keep these changes!

--- Chapter01/script2.sh
+++ Chapter01/script2.sh
@@ -1,3 +1,8 @@
 # The script2.sh script
 #!/bin/bash
 echo $name
+
+# second part
+
+name=shell # it updated the value of name variable but didn't update the original which comes from script1.
+echo $name
